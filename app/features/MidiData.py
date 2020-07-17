466443646ea3753e945ef3f718351564ec9fcd17
Armand Mégrot <armand.megrot@gmail.com>
2020-07-17 18:45:16 +0200

    Fix tempo using ticks instead of seconds

--- app/features/MidiData.py
+++ app/features/MidiData.py
@@ -2,6 +2,7 @@ from mido import MidiFile, tempo2bpm, tick2second, merge_tracks
 from app.features.Type.Notes import Notes
 from app.features.Type.Tempos import Tempos
 
+DEFAULT_TEMPO = 500000
 
 class MidiData:
     def __init__(self, file_name):
@@ -13,16 +14,19 @@ class MidiData:
     def parseData(self, file_name):
         mid = MidiFile(file_name)
         currentTime = 0
-        tickTime = 0
-        tempoTemp = 0
+        tempo = DEFAULT_TEMPO
         for msg in merge_tracks(mid.tracks):
-            tickTime += msg.time
-            currentTime += tick2second(msg.time, mid.ticks_per_beat, tempoTemp)
+            if msg.time > 0:
+                delta = tick2second(msg.time, mid.ticks_per_beat, tempo)
+            else:
+                delta = 0
+            currentTime += delta
+
             if msg.type == 'set_tempo':
-                tempoTemp = msg.tempo
+                tempo = msg.tempo
                 # fill tempo
                 self.tempos.append(msg.tempo, mid.ticks_per_beat, msg.time, currentTime)
             elif msg.type == 'note_on':
                 self.notes.append(msg.note, currentTime)
-        self.notes.setTotalTime(tickTime)
-        self.tempos.setTotalTime(tickTime)
+        self.notes.setTotalTime(currentTime)
+        self.tempos.setTotalTime(currentTime)
