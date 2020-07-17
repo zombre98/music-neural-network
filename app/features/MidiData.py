d9a975feed11433db329f44e245b4513048030d4
Armand Mégrot <armand.megrot@gmail.com>
2020-07-17 17:31:47 +0200

    Remove midi file instance variable to reduce memory usage

--- app/features/MidiData.py
+++ app/features/MidiData.py
@@ -5,22 +5,24 @@ from app.features.Type.Tempos import Tempos
 
 class MidiData:
     def __init__(self, file_name):
-        self.mid = MidiFile(file_name)
         self.tempos = Tempos()
         self.notes = Notes()
 
-        self.parseData()
+        self.parseData(file_name)
 
-    def parseData(self):
-        self.notes.setTotalTime(self.mid.length)
-        self.tempos.setTotalTime(self.mid.length)
+    def parseData(self, file_name):
+        mid = MidiFile(file_name)
         currentTime = 0
+        tickTime = 0
         tempoTemp = 0
-        for msg in merge_tracks(self.mid.tracks):
-            currentTime += tick2second(msg.time, self.mid.ticks_per_beat, tempoTemp)
+        for msg in merge_tracks(mid.tracks):
+            tickTime += msg.time
+            currentTime += tick2second(msg.time, mid.ticks_per_beat, tempoTemp)
             if msg.type == 'set_tempo':
                 tempoTemp = msg.tempo
                 # fill tempo
-                self.tempos.append(msg.tempo, self.mid.ticks_per_beat, msg.time, currentTime)
+                self.tempos.append(msg.tempo, mid.ticks_per_beat, msg.time, currentTime)
             elif msg.type == 'note_on':
                 self.notes.append(msg.note, currentTime)
+        self.notes.setTotalTime(tickTime)
+        self.tempos.setTotalTime(tickTime)
